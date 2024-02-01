f209ccca3eb49a562964e9d6b74dd4814d547992
Thomas Habte <tmhabte@gmail.com>
2024-02-01 13:01:11 -0800

    k div by kuhn lenght fix

--- chrom_stab/sf2_mat_calc_shlk.py
+++ chrom_stab/sf2_mat_calc_shlk.py
@@ -1,12 +1,6 @@
 import numpy as np
 from chromo_vertex_low_mem import *
 
-klog_min = -3
-klog_max = -1
-klog_num = 30
-k_vec = np.logspace(-3, -1, 30)
-
-
 mu_max = 0 #10
 mu_min = -10
 del_mu = 1
@@ -15,13 +9,13 @@ del_mu = 1
 n_bind = 2 #types of proteins/marks
 
 
-chi = 0
+chi = None
 e_m = np.array([-1.5, -1.5]) #binding energy
 
-
+v_int =  np.array([[-3.92, 3], [3, -3.92]])
 # v_int = np.array([[-3.92,0],[0,-3.92]]) # protein-protein interaction param
 # v_int = np.array([[0,3.92],[3.92,0]]) # protein-protein interaction param
-v_int = np.array([[0,0],[0,0]]) # protein-protein interaction param
+# v_int = np.array([[0,0],[0,0]]) # protein-protein interaction param
 
 
 phi_c = 0.6# avg amount of chromosome
@@ -37,15 +31,21 @@ marks_2 = np.loadtxt(r"H3K27me3_ENCFF470ECE_Chr_22_trimmed.txt").astype(np.uint8
 #marks_1 = [0,2,1,0,2,1] # marks for protein 1
 #marks_2 = [1,1,1,0,2,2] # marks for protein 2
 
-ID = np.sum(e_m) + np.sum(v_int) + phi_c + mu_max + mu_min + del_mu + klog_min + klog_max + klog_num//5
-ID = np.round(ID, 5)
-
 poly_marks = [marks_1, marks_2]
 
 chrom = def_chrom(n_bind, v_int, chi, e_m, phi_c, poly_marks, mu_max, mu_min, del_mu, chrom_type)
 
+[n_bind, v_int, chi, e_m, phi_c, poly_marks, mu_max, mu_min, del_mu, f_om, N, N_m, b] = chrom
 
+klog_min = -2.5
+klog_max = -1
+klog_num = 30
+k_vec = np.logspace(klog_min, klog_max, klog_num) / (b)
+
+ID = np.sum(e_m) + np.sum(v_int) + phi_c + mu_max + mu_min + del_mu + klog_min + klog_max + klog_num//5
+ID = np.round(ID, 5)
 np.save(r"ID=%s_settings" % ID, [chrom, [klog_min, klog_max, klog_num]])
+print("saved settings file")
 
 f_gam, s_bind = calc_binding_states(chrom)
 
