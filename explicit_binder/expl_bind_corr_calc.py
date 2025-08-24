055b7e9f03aafbf060072b65fd4c6bd8a01f9351
Thomas Habte <tmhabte@gmail.com>
2025-08-23 18:15:02 -0700

    S3_AAA

--- explicit_binder/expl_bind_corr_calc.py
+++ explicit_binder/expl_bind_corr_calc.py
@@ -58,6 +58,11 @@ def calc_sf2(psol, corrs, k):
     phi_A = psol.phi_A
     phi_B = psol.phi_B
 
+    phi_Ab = psol.phi_Ab
+    phi_Au = psol.phi_Au
+    phi_Bb = psol.phi_Bb
+    phi_Bu = psol.phi_Bu
+
     sA, sB = corrs
     sAsA = np.outer(sA, sA)
     sBsB = np.outer(sB, sB)
@@ -161,19 +166,36 @@ def calc_sf2(psol, corrs, k):
 #           [0, 0, 0, 0, S_BuBu*N_B, 0],\
 #           [0, 0, 0, 0, 0, S_ss]]
 
-    # P A_tot B_tot S    
-    S2 = [[S_PP*phi_p*N_P, S_AP*phi_p*N_A, S_BP*phi_p*N_B, 0,], \
-          [S_AP*phi_p*N_A, S_AA*(phi_p*N_A**2)/N_P + S_AuAu*N_A, S_AB*(phi_p*N_A*N_B)/N_P, 0],\
-          [S_BP*phi_p*N_B, S_AB*(phi_p*N_A*N_B)/N_P, S_BB*(phi_p*N_B**2)/N_P + S_BuBu*N_B, 0],\
-          [0, 0, 0, S_ss]]
-    return S2  
+    # P A_tot B_tot S  OLD unbound weighting
+    # S2 = [[S_PP*phi_p*N_P, S_AP*phi_p*N_A, S_BP*phi_p*N_B, 0,], \
+    #       [S_AP*phi_p*N_A, S_AA*(phi_p*N_A**2)/N_P + S_AuAu*N_A, S_AB*(phi_p*N_A*N_B)/N_P, 0],\
+    #       [S_BP*phi_p*N_B, S_AB*(phi_p*N_A*N_B)/N_P, S_BB*(phi_p*N_B**2)/N_P + S_BuBu*N_B, 0],\
+    #       [0, 0, 0, S_ss]]
+    # return S2  
 
-#     # "intuitive" AuAu, BuBu sfs
+#     # "intuitive" but largely correct AuAu, BuBu sfs- get a factor of phi from saddle point eqn
 #     S2 = [[S_PP*phi_p*N_P, S_AP*phi_p*N_A, S_BP*phi_p*N_B, 0,], \
 #           [S_AP*phi_p*N_A, S_AA*(phi_p*N_A**2)/N_P + S_AuAu*phi_A*N_A, S_AB*(phi_p*N_A*N_B)/N_P, 0],\
 #           [S_BP*phi_p*N_B, S_AB*(phi_p*N_A*N_B)/N_P, S_BB*(phi_p*N_B**2)/N_P + S_BuBu*phi_B*N_B, 0],\
 #           [0, 0, 0, S_ss]]
-#     return S2  
+#     return S2
+
+
+    # # folowing algebra AuAu, BuBu sfs- get a factor of phi from saddle point eqn
+    # # also have N_A^2 for unbound sfs. NOT THAT GOOD
+    # S2 = [[S_PP*phi_p*N_P, S_AP*phi_p*N_A, S_BP*phi_p*N_B, 0,], \
+    #       [S_AP*phi_p*N_A, S_AA*(phi_p*N_A**2)/N_P + S_AuAu*phi_A*N_A**2, S_AB*(phi_p*N_A*N_B)/N_P, 0],\
+    #       [S_BP*phi_p*N_B, S_AB*(phi_p*N_A*N_B)/N_P, S_BB*(phi_p*N_B**2)/N_P + S_BuBu*phi_B*N_B**2, 0],\
+    #       [0, 0, 0, S_ss]]
+    # return S2
+
+    # applying saddle point result to bound and unbound sfs- NOT ALGEBRAICALLY FOUNDED
+    S2 = [[S_PP*phi_p*N_P, S_AP*phi_p*N_A, S_BP*phi_p*N_B, 0,], \
+          [S_AP*phi_p*N_A, S_AA*phi_Ab*N_A + S_AuAu*phi_Au*N_A, S_AB*phi_Ab*N_B, 0],\
+          [S_BP*phi_p*N_B, S_AB*phi_Ab*N_B, S_BB*phi_Bb*N_B + S_BuBu*phi_Bu*N_B, 0],\
+          [0, 0, 0, S_ss]]
+    return S2
+
 
 #     print("IGNORNING UNBOUND POLY")
 #     S2 = [[S_PP*phi_p*N_P, S_AP*phi_p*N_A, S_BP*phi_p*N_B, 0,], \
@@ -191,4 +213,287 @@ def calc_sf2(psol, corrs, k):
     return S2, S_AuAu*N_A, S_BuBu*N_B
     # delta = j1 - j2
 
+
+# define a set of integral functions (e.g. S_AAA^(3,1)), and create a function that, when given appropriate
+# integral functions for (3,1), (3,2), and (3,3) and the k and b and corr identities, returns the sf3
+
+import numpy as np
+
+def S_AAA31(k_alp, k_bet, b, N_A):
+    """
+    j3 = j2 = j1
+    Triple integral:
+    I = ∫_0^N dn3 ∫_0^n3 dn2 ∫_0^n2 dn1 
+        exp[-la (n3-n2) - mu (n2-n1)]
     
+    Parameters
+    ----------
+    la : float
+        λ = (b_A^2/6) * k_α^2
+    mu : float
+        μ = (b_A^2/6) * k_β^2
+    N : float
+        Chain length N_A
+    
+    Returns
+    -------
+    float
+        Value of the integral
+    """
+    x1 = (b**2/6) * k_alp**2
+    x2 = (b**2/6) * k_bet**2
+
+    # Handle λ ≈ μ with a tolerance
+    if np.isclose(x1, x2, atol=1e-12):
+        return (N_A/x1**2 
+                + (N_A*x1**2*np.exp(-x1*N_A) - 2*x1*(1 - np.exp(-x1*N_A)))/x1**4)
+    else:
+        return (N_A/(x1*x2) 
+                + (1 - np.exp(-x1*N_A))/(x1**2*(x1 - x2)) 
+                - (1 - np.exp(-x2*N_A))/(x2**2*(x1 - x2)))
+
+import numpy as np
+
+def S_AAA32(k2, k3, bA, bP, N_A, N_P, M, j3, j1):
+    """
+    Compute
+    I = 2 * ∫_0^{N_A} dn3 ∫_0^{N_A} dn2 ∫_0^{n2} dn1 exp[-a*k3^2*n1 - a*k2^2*(n2-n1)
+                                                     - C*k3^2 - a*k3^2*n3]
+    where a = bA^2/6, C = (1/6)*(Nchain/(M-1))*bP^2 * (j3-j1).
+    Parameters:
+      k2, k3   : wavevector magnitudes (floats)
+      bA       : Kuhn length on A side chains
+      bP       : backbone Kuhn length
+      N_P   : N in backbone prefactor (from user expression)
+      M        : M in backbone prefactor (from user expression)
+      jdiff    : (j3 - j1) integer or float
+      N_A      : upper limit for n integrals
+    """
+    jdiff = j3 - j1
+    a = bA**2 / 6.0
+    C = (1.0/6.0) * (N_P / (M - 1.0)) * (bP**2) * jdiff
+
+    k2sq = float(k2**2)
+    k3sq = float(k3**2)
+    Delta = k3sq - k2sq
+
+    # n3 integral factor
+    if np.isclose(k3sq, 0.0):
+        A3 = N_A
+    else:
+        A3 = (1.0 - np.exp(-a * k3sq * N_A)) / (a * k3sq)
+
+    # double integral I12
+    tol = 1e-12
+    if np.isclose(Delta, 0.0, atol=tol):
+        # k2^2 == k3^2 == kappa
+        kappa = k3sq
+        if np.isclose(kappa, 0.0):
+            # both zero
+            I12 = 0.5 * N_A**2  # ∫_0^N dn2 ∫_0^{n2} dn1 1 = N^2/2
+        else:
+            I12 = (1.0 / (a * kappa)) * ( (1.0 - np.exp(-a * kappa * N_A)) / (a * kappa)
+                                         - (1.0 - np.exp(-2.0 * a * kappa * N_A)) / (2.0 * a * kappa) )
+    else:
+        # general case
+        term1 = (1.0 - np.exp(-a * k2sq * N_A)) / (a * k2sq) if not np.isclose(k2sq, 0.0) else N_A
+        term2 = (1.0 - np.exp(-a * k3sq * N_A)) / (a * k3sq) if not np.isclose(k3sq, 0.0) else N_A
+        I12 = (1.0 / (a * Delta)) * (term1 - term2)
+
+    I = 2.0 * np.exp(-C * k3sq) * A3 * I12
+    return I
+
+def S_AAA33(k1, k2, k3, bA, bP, N_A, N_P, M, j1, j2, j3):
+    """
+    Compute the triple integral:
+    I = ∫_0^N_A dn1 ∫_0^N_A dn2 ∫_0^N_A dn3 exp[ ... ]
+    with factorized exponent structure.
+    """
+    # Coefficients for chain contributions
+    a1 = (bA**2 / 6.0) * k1**2
+    a2 = (bA**2 / 6.0) * k2**2
+    a3 = (bA**2 / 6.0) * k3**2
+    
+    # Propagator prefactor
+    const = np.exp(
+        - (N_P / (6.0*(M-1))) * bP**2 * (k1**2 * (j2 - j1) + k3**2 * (j3 - j2))
+    )
+    
+    def f(a):
+        return (1 - np.exp(-a * N_A)) / a if a > 1e-14 else N_A
+    
+    return const * f(a1) * f(a2) * f(a3)
+
+def calc_sf3(psol, corrs, k, k2):
+    v_P = psol.v_p
+    N_P = psol.N_P
+    b_P = psol.b_P
+    v_A = psol.v_A
+    N_A = psol.N_A
+    b_A = psol.b_A
+    v_B = psol.v_B
+    N_B = psol.N_B
+    b_B = psol.b_B
+    M = psol.M
+    solv_cons = psol.solv_cons
+    phi_p = psol.phi_p
+    phi_A = psol.phi_A
+    phi_B = psol.phi_B
+
+    phi_Ab = psol.phi_Ab
+    phi_Au = psol.phi_Au
+    phi_Bb = psol.phi_Bb
+    phi_Bu = psol.phi_Bu
+
+    sA, sB = corrs
+    sAsA = np.outer(sA, sA)
+    sBsB = np.outer(sB, sB)
+    sAsB = np.outer(sA, sB)
+
+    sAsAsA = np.einsum("i,j,k->ijk", sA, sA, sA)
+
+    
+    x_p = (1/6)*N_P*b_P**2*k**2
+    x_A = (1/6)*N_A*b_A**2*k**2
+    x_B = (1/6)*N_B*b_B**2*k**2
+    x_del = (1/6)*(N_P/(M-1))*b_P**2*k**2
+    grid = np.indices((M,M,M))
+    j1 = grid[0]
+    j2 = grid[1] 
+    j3 = grid[2] 
+
+    S3_AAA = np.zeros(len(k))
+
+    for i in range(len(k)):
+        k_1 = k[i]
+        k_2 = k2[i]
+        k_12 = k_1 + k_2
+
+        # CASE 1; kA = k1 + k2, kB = k_1; S3 > S2 > S1 and S1 > S2 > S3
+        case1 = [[k_12, k_1], [j3, j2, j1]]
+        case1_deg = [[k_1, k_12], [j1, j2, j3]]
+
+        # CASE 2; kA = k2, kB = k1 + k2; S2 > S1 > S3 and S3 > S1 > S2
+        case2 = [[k_2, k_12], [j2, j1, j3]]
+        case2_deg = [[k_12, k_2], [j3, j1, j2]]
+        
+        # CASE 3; kA = k2, kB = -k1; S2 > S3 > S1 and S1 > S3 > S2
+        case3 = [[-k_2, k_1], [j2, j3, j1]] # SWITCHED negatives from -k_1
+        case3_deg = [[k_1, -k_2], [j1, j3, j2]] # SWITCHED negatives from -k_1
+        
+        case_arr = [case1, case2, case3, case1_deg, case2_deg, case3_deg]
+        # need to consider degenerate cases. flipping each element in array, then appending to original case array
+        # case_arr = np.vstack((case_arr, [[np.flipud(el) for el in cse] for cse in case_arr]))
+        
+#        for each case and sub case, add to a matrix C(j1, j2, j3) which contains the contribution to the overall S3
+#        then sum over all indices. Need to keep track of js so that appropriate multiplications with cross corr matrix M3        
+        C = np.zeros((M,M,M))
+
+        # S3_AAA = 0
+        for cse in case_arr:
+            kA, kB = cse[0]
+            ordered_js = cse[1]
+            
+            S3_AAA[i] += np.sum(sA*S_AAA31(kA, kB, b_A, N_A))
+            # S_AAA32(k2, k3, bA, bP, N_A, N_P, M, j3, j1)
+            index = (ordered_js[0] == ordered_js[1]) * (ordered_js[0] != ordered_js[-1])
+            I = S_AAA32(kA, kB, b_A, b_P, N_A, N_P, M, ordered_js[0], ordered_js[-1])
+            corr = sAsAsA / sA[ordered_js[0]] # <s_j1 s_j3> POSSIBLE SOURCE OF NANS np.nans. if so must do #corr= np.einsum("i,j,k->ijk", vals, np.ones(len(vals)), vals)
+            S3_AAA[i] += np.sum(corr[np.where(index != 0)]*I[np.where(index != 0)])
+
+            index = (ordered_js[0] != ordered_js[1]) * (ordered_js[0] != ordered_js[2]) * (ordered_js[1] != ordered_js[2])
+            # (k1, k2, k3, bA, bP, N_A, N_P, M, j1, j2, j3)
+            I = S_AAA33(kA, kB, -kA-kB, b_A, b_P, N_A, N_P, M, ordered_js[0], ordered_js[1], ordered_js[2])
+            S3_AAA[i] += np.sum(sAsAsA[np.where(index != 0)]*I[np.where(index != 0)])
+
+            # xm_A = (1/6) * N_m * b**2 * np.linalg.norm(kA)**2
+            # xm_B = (1/6) * N_m * b**2 * np.linalg.norm(kB)**2
+            
+            # C = calc_case_s3(C, xm_A, xm_B, ordered_js)
+
+
+def calc_case_s3(C, xm_A, xm_B, ordered_js):
+
+    jmax, jmid, jmin = ordered_js
+    
+    cylindrical = False
+    epsilon = 0.0000001
+    if xm_A + epsilon > xm_B and xm_A - epsilon < xm_B:
+        cylindrical = True
+    
+    xm_A_eq_0 = False
+    if xm_A < 1e-5:
+        xm_A_eq_0 = True
+        
+    xm_B_eq_0 = False
+    if xm_B < 1e-5:
+        xm_B_eq_0 = True
+
+    #for each sub case, looking at the degenerate case where 1 and 2 are switched
+    constant = np.exp(-xm_A*(jmax - jmid)) * np.exp(-xm_B*(jmid - jmin)) 
+
+    # sub case 1; jmax > jmid > jmin, {s1, s2, s3} any 
+    index = (jmax > jmid) * (jmid > jmin)
+    
+    if cylindrical == True:
+        integral = (1 / xm_A**2) * 2 * (-1 + np.cosh(xm_A))
+    elif xm_B_eq_0:
+        integral = (2*(-1+np.cosh(xm_A)))/ (xm_A**2)
+    elif xm_A_eq_0:
+        integral = (2*(-1+np.cosh(xm_B)))/ (xm_B**2)
+    else:
+        integral = (-2 / (xm_A * (xm_A - xm_B) * xm_B)) \
+        * (-np.sinh(xm_A) + np.sinh(xm_A - xm_B) + np.sinh(xm_B))
+
+    C[np.where((index) != 0)] += constant[np.where(index != 0)] \
+                                    * integral
+    
+    # sub case 2; jmax = jmid > jmin, s3 > s2, {s1} any
+    index = (jmax == jmid) * (jmid > jmin)
+    
+    if cylindrical == True:
+        integral = (1 / xm_A**3) *( (2 + xm_A) * (-1 + np.cosh(xm_A)) - (xm_A * np.sinh(xm_A)) )
+    elif xm_B_eq_0:
+        integral = (-1 + xm_A + np.cosh(xm_A) - np.sinh(xm_A))/ (xm_A**2)
+    elif xm_A_eq_0:
+        integral = (np.exp(-xm_B)*(-1 + np.exp(xm_B))*(1+np.exp(xm_B)*(-1 + xm_B))) / (xm_B**3)   
+    else:
+        integral = ((-1 + np.exp(xm_B))/(xm_A * (xm_A - xm_B)*xm_B**2)) \
+        * (xm_A + (-1 + np.exp(-xm_A))*xm_B - xm_A*np.cosh(xm_B) + xm_A*np.sinh(xm_B))
+
+    C[np.where((index) != 0)] += constant[np.where(index != 0)] \
+                                    * integral 
+
+    # BONUS sub case 4; jmax > jmid = jmin, s2 > s1, {s3} any 
+    index = (jmax > jmid) * (jmid == jmin)
+    
+    if cylindrical == True:
+        integral = (1 / xm_A**3) *( (2 + xm_A) * (-1 + np.cosh(xm_A)) - (xm_A * np.sinh(xm_A)) )
+    elif xm_B_eq_0:
+        integral = ((-2+xm_A)*(-1+np.cosh(xm_A))+ (xm_A*np.sinh(xm_A)))/ (xm_A**3)
+    elif xm_A_eq_0:
+        integral = (-1+xm_B+np.cosh(xm_B) - np.sinh(xm_B))/ (xm_B**2)
+    else:
+        integral = (((-1 + np.exp(xm_A))*(np.exp(-xm_A - xm_B)))/(xm_B * (xm_A - xm_B)*xm_A**2)) \
+        * (-np.exp(xm_A)*xm_A + np.exp(xm_A + xm_B) * (xm_A -xm_B) + np.exp(xm_B)*xm_B)
+
+    C[np.where((index) != 0)] += constant[np.where(index != 0)] \
+                                    * integral 
+
+    # sub case 3; jmax = jmid = jmin, s3 > s2 > s1
+    index = (jmax == jmid) * (jmid == jmin)
+
+    if cylindrical == True:
+        integral = (1 / xm_A**3) * (np.exp(-xm_A) * (2 + np.exp(xm_A)*(-2 + xm_A) + xm_A))
+    elif xm_B_eq_0:
+        integral = (2-2*np.exp(-xm_A) - 2*xm_A + xm_A**2)/ (2*xm_A**3)
+    elif xm_A_eq_0:
+        integral = (2-2*np.exp(-xm_B) - 2*xm_B + xm_B**2)/ (2*xm_B**3)
+    else:
+        integral = (1 / (xm_A**2 * xm_B - xm_A * xm_B**2))\
+        * ( xm_A + (((-1 + np.exp(-xm_B)) * xm_A)/(xm_B)) - xm_B + ((xm_B - np.exp(-xm_A)*xm_B)/(xm_A)) )
+
+    C[np.where(index != 0)] += 1\
+                                    * constant[np.where(index != 0)]\
+                                    * integral
+    return C
